25b3e8c424e5e61bdbb959a9617e99bfa63a1136
edward-nn <ed.negal@gmail.com>
2023-02-05 13:34:26 +0200

    add person

--- index.js
+++ index.js
@@ -1,102 +1,142 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
+var morgan = require('morgan')
+
+morgan.token('id', function getId (req) {
+  return req.id
+})
+
+
+app.use(assignId)
+
+app.use(express.json())
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :body'))
+
+function assignId (req, res, next) {
+    req.id = generateId()
   next()
 }
+
+
+
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000);
+  return id;
+}
+
+app.get('/api/persons', (request, response) => {
+    //console.log(request.headers)
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) =>{
+  const id = Number(request.params.id)
+  const person = persons.find(item => item.id === id)
+  
+  if(!person) {response.status(404).end()  
+  }   else {    
+    response.json(person)
+  }
+
+
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  //console.log('body', body)
+  if (!body){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  } else if(!body.name || !body.number){
+    return response.status(400).json({
+      error: 'The name or number is missing'
+    })     
+  } else if(persons.find(person => person.name === body.name)){
+    return response.status(400).json({
+      error: 'The name already exists in the phonebook'
+    })
+  }
+
+
+  //body = (...body, )
+  const id = generateId()
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number || false,
+  }
+  //console.log('person', person)
+  persons = persons.concat(person)
+  response.json(persons)
+})
+
+  app.delete('/api/persons/:id', (request, response) => {
+  if(!persons){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }   else { 
+    const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+  response.status(204).end()
+
+  }
+  })
+
+  
+
+app.get('/info', (request, response) =>{
+    
+    if (!persons){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    
+    response.send(`<span>Phonebook has info for ${persons.length} people.</span><p>${new Date()}</p>`);
+})
+
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
